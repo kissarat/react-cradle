d8d02812776aa42161d05b9362ffedf7947bf834
Taras Labiak <kissarat@gmail.com>
2019-08-16 06:14:32 +0300

    + generate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import faker from 'faker';
 import App from './App';
 
 const root = document.getElementById('root');
@@ -12,3 +13,16 @@ window.render = function render(props) {
 window.render({
     children: 'Project skeleton for demonstration capabilities of component in development'
 });
+
+function generateContent(props) {
+    return {
+        children: faker.lorem.text(),
+        ...props
+    }
+}
+
+window.faker = faker;
+
+window.generate = (props = {}) => window.render(generateContent(props));
+
+window.intervalGenerate = (props = {}) => setInterval(() => window.generate(props), 800);
