ae7e42011fe706c1bd8dc582f0313abc5209ad4d
Taras Labiak <kissarat@gmail.com>
2019-08-19 13:49:16 +0300

    + save state

--- deploy/dns.tf
+++ deploy/dns.tf
@@ -12,3 +12,11 @@ resource "aws_route53_record" "first" {
   ttl = 100
   records = ["${aws_eip.first.public_ip}"]
 }
+
+resource "aws_route53_record" "jenkins" {
+  name = "jenkins.${var.hostname}"
+  type = "A"
+  zone_id = "${aws_route53_zone.first.id}"
+  ttl = 100
+  records = ["${aws_eip.first.public_ip}"]
+}
---
+++ deploy/save.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+tar cvf archive/state.tar.gz deploy/first.pem deploy/terraform.tfstate
