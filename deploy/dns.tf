2e961018ca6b959c3017e83975125363f65c494a
Taras Labiak <kissarat@gmail.com>
2019-09-18 17:56:37 +0300

    + local domains

--- deploy/dns.tf
+++ deploy/dns.tf
@@ -15,16 +15,40 @@ resource "aws_route53_record" "first" {
 
 resource "aws_route53_record" "jenkins" {
   name = "jenkins.${var.hostname}"
-  type = "A"
+  type = "CNAME"
   zone_id = aws_route53_zone.first.id
   ttl = 100
-  records = [aws_eip.first.public_ip]
+  records = [var.hostname]
 }
 
-resource "aws_route53_record" "react-slick" {
-  name = "react-slick.${var.hostname}"
+resource "aws_route53_record" "local" {
+  name = "local.${var.hostname}"
   type = "A"
   zone_id = aws_route53_zone.first.id
   ttl = 100
-  records = [aws_eip.first.public_ip]
+  records = ["127.0.0.1"]
+}
+
+resource "aws_route53_record" "local_gateway" {
+  name = "gateway.local.${var.hostname}"
+  type = "A"
+  zone_id = aws_route53_zone.first.id
+  ttl = 100
+  records = ["192.168.0.1"]
+}
+
+resource "aws_route53_record" "local_second" {
+  name = "second.local.${var.hostname}"
+  type = "A"
+  zone_id = aws_route53_zone.first.id
+  ttl = 100
+  records = ["192.168.0.2"]
+}
+
+resource "aws_route53_record" "local_home" {
+  name = "home.local.${var.hostname}"
+  type = "A"
+  zone_id = aws_route53_zone.first.id
+  ttl = 100
+  records = ["192.168.1.1"]
 }
